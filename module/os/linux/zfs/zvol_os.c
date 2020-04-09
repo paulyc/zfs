a76c57e7359588885bcc464a3f77f2b010ed2992
paulyc <paulyc@quetzalcoatl.us>
2020-04-09 09:24:17 -0400

    fix for 5.7 pass queue request callback to blk_alloc_queue rather than via blk_queue_make_request

--- module/os/linux/zfs/zvol_os.c
+++ module/os/linux/zfs/zvol_os.c
@@ -772,11 +772,10 @@ zvol_alloc(dev_t dev, const char *name)
 
 	mutex_init(&zv->zv_state_lock, NULL, MUTEX_DEFAULT, NULL);
 
-	zv->zv_zso->zvo_queue = blk_alloc_queue(GFP_ATOMIC);
+	zv->zv_zso->zvo_queue = blk_alloc_queue(zvol_request, GFP_ATOMIC);
 	if (zv->zv_zso->zvo_queue == NULL)
 		goto out_kmem;
 
-	blk_queue_make_request(zv->zv_zso->zvo_queue, zvol_request);
 	blk_queue_set_write_cache(zv->zv_zso->zvo_queue, B_TRUE, B_TRUE);
 
 	/* Limit read-ahead to a single page to prevent over-prefetching. */
